01ef29f3fb22ce6829201aa50965bf5470c6d5b2
Eccenux <eccenux+git@gmail.com>
2017-06-10 02:48:42 +0200

    more cols

--- .sql/__TABLES.sql
+++ .sql/__TABLES.sql
@@ -5,6 +5,9 @@ DROP TABLE IF EXISTS profile;
 CREATE TABLE profile (
 	id	int UNSIGNED NOT NULL AUTO_INCREMENT,
 	
+	`dt_create`		DATETIME,
+	`dt_change`		DATETIME,
+
 	`group_name`	varchar(20),
 	`invites_no`	int,
 	
@@ -26,13 +29,26 @@ CREATE TABLE profile (
 DROP TABLE IF EXISTS personal;
 CREATE TABLE personal (
 	id int UNSIGNED NOT NULL AUTO_INCREMENT,
+
+	`dt_create`		DATETIME,
+	`dt_change`		DATETIME,
 	
-	`sex`		varchar(100),
-	`age`		int,
-	`region`	varchar(200),
+	`sex`			varchar(20),
+	`age`			int,
+	`region`		varchar(200),
+
+	`pesel`			varchar(12),
 	
-	`csv_row`	text,
-	`csv_file`	int UNSIGNED,
+	`name`			varchar(255),
+	`surname`		varchar(255),
+	`city`			varchar(255),
+	`street`		varchar(255),
+	`building_no`	varchar(10),
+	`flat_no`		varchar(10),
+	`zip_code`		varchar(10),
+	
+	`csv_row`		text,
+	`csv_file`		int UNSIGNED,
 	
 	PRIMARY KEY (id),
 	KEY (csv_file)
@@ -45,9 +61,12 @@ DROP TABLE IF EXISTS file;
 CREATE TABLE file (
 	id int UNSIGNED NOT NULL AUTO_INCREMENT,
 	
-	`type`		varchar(20),	-- 'profile' / 'personal' (?)
-	`name`		varchar(200),
-	`dt_upload` DATETIME,
+	`dt_create`		DATETIME,
+	`dt_change`		DATETIME,
+
+	`type`			varchar(20),	-- 'profile' / 'personal' (?)
+	`column_map`	text,			-- column mapping e.g. 'sex:2,\nage:3,\nregion:1,\n...'
+	`name`			varchar(200),
 
 	`contents`	LONGTEXT,
 	
