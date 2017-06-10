76fd4a6936bf165ded18c0993f245f1ff1b3a876
Eccenux <eccenux+git@gmail.com>
2017-06-10 02:50:21 +0200

    basic db classes

--- inc/db/personal.php
+++ inc/db/personal.php
@@ -19,7 +19,7 @@ class dbPersonal extends dbBaseClass
 	 * @see dbBaseClass
 	 * @var string
 	 */
-	protected $pv_defaultOrderSql = 'ORDER BY nazwisko, imie';
+	protected $pv_defaultOrderSql = 'ORDER BY pesel';
 
 	/**
 	 * @see dbBaseClass
@@ -27,15 +27,68 @@ class dbPersonal extends dbBaseClass
 	 */
 	protected $pv_aliasNames2colNames = array (
 		'id' => 'id',
-		'dt' => 'dt',
-		'ankieta_id' => 'ankieta_id',
 		
-		'nazwisko_imie' => 'CONCAT(nazwisko, \', \', imie)',
-		'imie' => 'imie',
-		'nazwisko' => 'nazwisko',
-		'nr_tel' => 'nr_tel',
-		'e_mail' => 'e_mail',
+		'dt_create' => 'dt_create',
+		'dt_change' => 'dt_change',
+
+		'sex' => 'sex',
+		'age' => 'age',
+		'region' => 'region',
+
+		'pesel' => 'pesel',
+		
+		'name' => 'name',
+		'surname' => 'surname',
+		'city' => 'city',
+		'street' => 'street',
+		'building_no' => 'building_no',
+		'flat_no' => 'flat_no',
+		'zip_code' => 'zip_code',
+
+		'csv_row' => 'csv_row',
+		'csv_file' => 'csv_file',
 	);
+
+	/**
+	 * Aliased names of columns that are to be parsed as integers.
+	 *
+	 * @note All other columns are parsed as string/binary so this is purely optional.
+	 *
+	 * @var array
+	 */
+	protected $pv_intColumnsByAlias = array('age');
+
+	/**
+	 * Alised names of columns that are to be excluded when inserting records.
+	 *
+	 * @note For tables that have automatically incremented ids you should add the name of this id column here.
+	 *
+	 * @see pf_insRecord()
+	 * @var array
+	 */
+	protected $pv_insertExcludedCols = array('id');
+
+	/**
+	 * Extra operations on a record to be run in `pf_insRecord`.
+	 *
+	 * @param array $pv_record The record.
+	 */
+	protected function pf_insRecordExtraParse(&$pv_record)
+	{
+		$now = date('Y-m-d H:i:s');
+		$pv_record['dt_create'] = $now;
+		$pv_record['dt_change'] = $now;
+	}
+	/**
+	 * Extra operations on a record to be run in `pf_setRecords`.
+	 *
+	 * @param array $pv_record The record.
+	 */
+	protected function pf_setRecordExtraParse(&$pv_record)
+	{
+		$now = date('Y-m-d H:i:s');
+		$pv_record['dt_change'] = $now;
+	}
 }
 
 ?>
--- inc/db/profile.php
+++ inc/db/profile.php
@@ -19,7 +19,7 @@ class dbProfile extends dbBaseClass
 	 * @see dbBaseClass
 	 * @var string
 	 */
-	protected $pv_defaultOrderSql = 'ORDER BY dt';
+	protected $pv_defaultOrderSql = 'ORDER BY id';
 
 	/**
 	 * @see dbBaseClass
@@ -27,86 +27,60 @@ class dbProfile extends dbBaseClass
 	 */
 	protected $pv_aliasNames2colNames = array (
 		'id' => 'id',
-		'dt' => 'dt',
-		'ankieta_id' => 'ankieta_id',
-		'grupa' => 'grupa',
 		
-		'miejsce' => 'miejsce',
-		'plec' => 'plec',
-		'rok' => 'rok',
-		'wiek' => '2017 - rok',
-		'wyksztalcenie' => 'wyksztalcenie',
+		'dt_create' => 'dt_create',
+		'dt_change' => 'dt_change',
+
+		'group_name' => 'group_name',
+		'invites_no' => 'invites_no',
+
+		'sex' => 'sex',
+		'age_min' => 'age_min',
+		'age_max' => 'age_max',
+		'region' => 'region',
+
+		'csv_row' => 'csv_row',
+		'csv_file' => 'csv_file',
 	);
 
 	/**
-	 * "Szablony" SQL do statystyk/szybkich zapytań.
+	 * Aliased names of columns that are to be parsed as integers.
 	 *
-	 * @see dbBaseClass->pf_getStats
+	 * @note All other columns are parsed as string/binary so this is purely optional.
+	 *
 	 * @var array
 	 */
-	protected $pv_sqlStatsTpls = array (
-		// Zbiorcze podsumowanie
-		'miejsce' =>
-			'SELECT miejsce, count(miejsce) as licznik
-			FROM profile
-			WHERE {pv_constraints|(1)}
-			GROUP BY miejsce
-			ORDER BY 1, 2',
-		'wyksztalcenie' =>
-			'SELECT wyksztalcenie, count(wyksztalcenie) as licznik
-			FROM profile
-			WHERE {pv_constraints|(1)}
-			GROUP BY wyksztalcenie
-			ORDER BY 1, 2',
-		'plec' =>
-			'SELECT plec as `płeć`, count(plec) as licznik
-			FROM profile
-			WHERE {pv_constraints|(1)}
-			GROUP BY plec
-			ORDER BY 1, 2',
-		'wiek' =>
-			'
-			(
-			SELECT \'18-24\' as wiek, count(*) as licznik
-			FROM profile
-			WHERE {pv_constraints|(1)} AND (2017 - rok <= 24)
-			)
-			UNION
-			(
-			SELECT \'25-39\', count(*) as licznik
-			FROM profile
-			WHERE {pv_constraints|(1)} AND (2017 - rok >=25 AND 2017 - rok <= 39)
-			)
-			UNION
-			(
-			SELECT \'40-64\', count(*) as licznik
-			FROM profile
-			WHERE {pv_constraints|(1)} AND (2017 - rok >=40 AND 2017 - rok <= 64)
-			)
-			UNION
-			(
-			SELECT \'65+\', count(*) as licznik
-			FROM profile
-			WHERE {pv_constraints|(1)} AND (2017 - rok >=65)
-			)
-		'
-	);
+	protected $pv_intColumnsByAlias = array('age_min', 'age_max');
 
 	/**
-	 * Grupy/statusy
+	 * Alised names of columns that are to be excluded when inserting records.
+	 *
+	 * @note For tables that have automatically incremented ids you should add the name of this id column here.
+	 *
+	 * @see pf_insRecord()
 	 * @var array
 	 */
-	public static $pv_grupy = array (
-		'w puli', 'grupa główna', 'zastępcza', 'rezerwowa', 'rez.zast.', 'rezygnacja'
-	);
+	protected $pv_insertExcludedCols = array('id');
 
-	public static function pf_wyksztalcenieTranslate($dbValue) {
-		switch ($dbValue)
-		{
-			 case 'p': return "podstawowe";
-			 case 's': return "średnie";
-			 case 'w': return "wyższe";
-		}
-		return $dbValue;
+	/**
+	 * Extra operations on a record to be run in `pf_insRecord`.
+	 *
+	 * @param array $pv_record The record.
+	 */
+	protected function pf_insRecordExtraParse(&$pv_record)
+	{
+		$now = date('Y-m-d H:i:s');
+		$pv_record['dt_create'] = $now;
+		$pv_record['dt_change'] = $now;
+	}
+	/**
+	 * Extra operations on a record to be run in `pf_setRecords`.
+	 *
+	 * @param array $pv_record The record.
+	 */
+	protected function pf_setRecordExtraParse(&$pv_record)
+	{
+		$now = date('Y-m-d H:i:s');
+		$pv_record['dt_change'] = $now;
 	}
 }
