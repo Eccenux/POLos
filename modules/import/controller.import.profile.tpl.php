52d01f61e021d92c59b86254ee6490e5df73360c
Eccenux <eccenux+git@gmail.com>
2017-06-14 22:42:25 +0200

    split style & and js; add section

--- modules/import/controller.import.profile.tpl.php
+++ modules/import/controller.import.profile.tpl.php
@@ -13,62 +13,35 @@
 <h3>Nowe dane</h3>
 
 <style>
-	label { font-weight: bold; }
+	<?php include 'controller.import.css'; ?>
 </style>
 
-<p><label for="import-csv">Plik CSV:</label> <input type="file" id="import-csv" /></p>
+<section>
+	<p><label for="import-csv">Plik CSV:</label> <input type="file" id="import-csv" /></p>
+</section>
 
-<p><label>Format pliku (kolejność kolumn):</label></p>
-<script>
-$( function() {
-	$( "#import-sortable-columns" ).sortable({
-		revert: true
-	});
-	// connect to columns
-	$( "#import-draggable-column" ).draggable({
-		connectToSortable: "#import-sortable-columns",
-		helper: "clone",
-		revert: "invalid"
-	});
-	// allow dropping columns back
-	// (but only with `ignore-column` class)
-	$('#import-draggable-column').droppable({
-		drop: function(event, ui) {
-			if (ui.draggable.hasClass('ignore-column')) {
-				ui.draggable.remove();
-			}
-		}
-	});
-	$(".sortable-columns").disableSelection();
-} );
-</script>
-<style>
-	ul.sortable-columns { list-style-type: none; margin: 0; padding: 0; margin-bottom: 10px; }
-	ul.sortable-columns li { margin: 5px; padding: 5px; width: 150px; }
-
-	ul#import-sortable-columns {
-		list-style-type: upper-latin;
-		margin-left: 1em;
-	}
-</style>
-
-<div style="float:left">
-	<ul id="import-sortable-columns" class="sortable-columns">
-		<?php foreach ($tplData['columns'] as $number => $column) { ?>
-			<?php if (empty($column)) { ?>
-				<li class="ui-state-highlight ignore-column"><em>ignoruj</em></li>
-			<?php } else { ?>
-				<li class="ui-state-default"
-					id="import-column-<?=$column['column']?>"
-				><?=$column['title']?></li>
+<section>
+	<p><label>Format pliku (kolejność kolumn):</label></p>
+	<script>
+		<?php include 'controller.import.columns.js'; ?>
+	</script>
+	<div style="float:left">
+		<ul id="import-sortable-columns" class="sortable-columns">
+			<?php foreach ($tplData['columns'] as $number => $column) { ?>
+				<?php if (empty($column)) { ?>
+					<li class="ui-state-highlight ignore-column"><em>ignoruj</em></li>
+				<?php } else { ?>
+					<li class="ui-state-default"
+						id="import-column-<?=$column['column']?>"
+					><?=$column['title']?></li>
+				<?php } ?>
 			<?php } ?>
-		<?php } ?>
-	</ul>
-</div>
-<div style="float:left">
-	<ul class="sortable-columns">
-		<li id="import-draggable-column" class="ui-state-highlight ignore-column"><em>ignoruj</em></li>
-	</ul>
-</div>
-
-<br clear="all"/>
+		</ul>
+	</div>
+	<div style="float:left">
+		<ul class="sortable-columns">
+			<li id="import-draggable-column" class="ui-state-highlight ignore-column"><em>ignoruj</em></li>
+		</ul>
+	</div>
+	<br clear="all"/>
+</section>
