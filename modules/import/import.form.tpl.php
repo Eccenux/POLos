07198e03fad4ce5ceee92c8a3a40f4b8eed2a496
Eccenux <eccenux+git@gmail.com>
2017-06-14 23:38:12 +0200

    split part two

--- modules/import/import.form.tpl.php
+++ modules/import/import.form.tpl.php
@@ -1,22 +1,6 @@
-<h2>Import profili</h2>
-
-<h3>Zaimportowane dane</h3>
-<?php if (empty($tplData['profile']['total'][0]['profiles'])) { ?>
-<p>Brak</p>
-<?php } else { ?>
-<ul>
-<li>Liczba profili: <?=$tplData['profile']['total'][0]['profiles']?></li>
-<li>Liczba zaproszeń do wylosowania: <?=$tplData['profile']['total'][0]['invites']?></li>
-</ul>
-<?php } ?>
-
-<h3>Nowe dane</h3>
-
 <style>
 	<?php include 'form.css'; ?>
 </style>
-
-<form id="import-form" method="post" action="">
 
 <section>
 	<p><label for="import-csv">Plik CSV:</label> <input type="file" id="import-csv" /></p>
@@ -49,11 +33,6 @@
 	<br clear="all"/>
 </section>
 
-<section class="buttons">
-	<input type="submit" name="save" value="Importuj" />
-</section>
-
-</form>
 <script>
 	<?php include 'import.form.js'; ?>
 </script>
