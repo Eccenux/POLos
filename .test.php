bae40699662303b04d672acb25947df778a35c08
Eccenux <eccenux+git@gmail.com>
2017-06-15 20:00:13 +0200

    Range parsing test

--- .test.php
+++ .test.php
@@ -7,6 +7,7 @@ date_default_timezone_set('Europe/Paris');
 
 require_once './modules/import/CsvParser.php';
 
+/**
 $dir = 'D:\_Varia\OkoloWWW\_moje - varia\PHP\POLos\.tests\modules\import';
 $profileCsv = $dir.'\profile.csv';
 
@@ -18,3 +19,20 @@ $firstRow = $parser->rows[CsvRowState::OK][0];
 $parsedColumnsCount = count($firstRow);
 $expectedColumnsCount = count($profileOrder) - 2;
 var_dump($parsedColumnsCount, $expectedColumnsCount);
+/**/
+
+$values = array(
+	'123-345' => array('min'=>'123', 'max'=>'345'),
+	'123,345' => array('min'=>'123', 'max'=>'345'),
+	'123, 345' => array('min'=>'123', 'max'=>'345'),
+	'345+' => array('min'=>'345'),
+);
+foreach ($values as $value => $expected)
+{
+	$matches = array();
+	preg_match("#(?P<min>\\d+)(?:[^\\d]*\\+|[^\\d]+(?P<max>\\d+))#", $value, $matches);
+	//preg_match("#(?P<min>\\d+)[^\\d]+(?P<max>\\d+)#", $value, $matches);
+	unset($matches[0], $matches[1], $matches[2]);
+	echo "\n";
+	var_export($matches);
+}
