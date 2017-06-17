cdb6d2fae6495907b3d1666952c660de287ebfd4
Eccenux <eccenux+git@gmail.com>
2017-06-17 20:42:41 +0200

    invites must be a number higher then 0

--- modules/import/CsvParser.profile.php
+++ modules/import/CsvParser.profile.php
@@ -3,8 +3,16 @@
 		@file Parser data.
 	*/
 	$columnParsers = array();
+	// must be a number higher then 0
 	$columnParsers['invites_no'] = function($name, $value){
-		return CsvParser::parseColumnInteger($name, $value);
+		//return CsvParser::parseColumnInteger($name, $value);
+		$number = intval($value, 10);
+		return array(
+			'state' => is_numeric($value) && ($number > 0) ? CsvRowState::OK : CsvRowState::INVALID,
+			'columns' => array(
+				$name => $number,
+			)
+		);
 	};
 	$columnParsers['age_min_max'] = function($name, $value){
 		return CsvParser::parseColumnRange('age_', $value);
