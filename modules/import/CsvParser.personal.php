fe6b6c21fa9e2a60543da1f14de9898a50b45b7c
Eccenux <eccenux+git@gmail.com>
2017-06-18 01:36:55 +0200

    Get sex and age form PESEL.

--- modules/import/CsvParser.personal.php
+++ modules/import/CsvParser.personal.php
@@ -1,8 +1,23 @@
 <?php
-	/**
-		@file Parser data.
-	*/
+/*
+	@file Parser data.
+*/
+	require_once 'PeselParser.php';
+
 	$columnParsers = array();
-	$columnParsers['region'] = $columnParsers['pesel'] = function($name, $value){
+	$columnParsers['pesel'] = function($name, $pesel){
+		$ret = array(
+			'state' => CsvRowState::INVALID,
+			'columns' => array(),
+		);
+		if (PeselParser::validatePesel($pesel)) {
+			$ret['state'] = CsvRowState::OK;
+			$ret['columns']['pesel'] = $pesel;
+			$ret['columns']['sex'] = PeselParser::sexFromPesel($pesel);
+			$ret['columns']['age'] = PeselParser::ageFromPesel($pesel);
+		}
+		return $ret;
+	};
+	$columnParsers['region'] = function($name, $value){
 		return CsvParser::parseColumnRequired($name, $value);
 	};
