e5eb1aa0305c477893ea5ea8d6853663e3130999
Eccenux <eccenux+git@gmail.com>
2017-06-18 16:38:03 +0200

    profiles draw ratio info

--- modules/draw/controller.summary.tpl.php
+++ modules/draw/controller.summary.tpl.php
@@ -1,9 +1,13 @@
 <h2>Podstawowa weryfikacja</h2>
 
+<style>
+	<?php include 'validation.css'; ?>
+</style>
+
 <?php if (!empty($tplData['drawValidationMessage'])) { ?>
-<div class="message <?=($tplData['drawPossible'] ? 'warning' : 'error')?>">
-<?=$tplData['drawValidationMessage']?>
-</div>
+	<div class="message <?=($tplData['drawPossible'] ? 'warning' : 'error')?>">
+	<?=$tplData['drawValidationMessage']?>
+	</div>
 <?php } ?>
 
 <h2>Dopasowanie profili</h2>
@@ -12,7 +16,7 @@
 <?=$tplData['drawMatchingMessage']?>
 </div>
 <?php } ?>
-<div>
+<div style="float:left">
 <?php
 	ModuleTemplate::printArray($tplData['profile-persons'], array(
 		'group_name' => 'grupa',
@@ -22,6 +26,16 @@
 		'region' => 'dzielnica',
 		'invites_no' => 'l. zaproszeń',
 		'persons' => 'l. osób',
+		'percentage' => 'os./zap.',
 	));
 ?>
+</div>
+<div style="float:left; padding-left: 1em;">
+	<table>
+		<tr><th>Legenda</th></tr>
+		<tr class="draw-fair">			<td>Losowanie sprawiedliwe</td></tr>
+		<tr class="draw-possible">		<td>Losowanie możliwe</td></tr>
+		<tr class="draw-minimum">		<td>Na styk</td></tr>
+		<tr class="draw-not-possible">	<td>Za mało osób</td></tr>
+	</table>
 </div>
