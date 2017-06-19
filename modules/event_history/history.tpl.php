9b964e808b1449d4d015a402e5f8fdcbfb90165d
Eccenux <eccenux+git@gmail.com>
2017-06-19 02:21:24 +0200

    event history will be differnt

--- modules/event_history/history.tpl.php
+++
@@ -1,3 +0,0 @@
-<h2>Historia działań</h2>
-<div class="draw-history draw-history-full"></div>
-<p><input type="button" name="history-delete" value="Wyczyść historię" onclick="drawHistory.deleteAll()" /></p>
