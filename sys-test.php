33376bb9b9763145120dff0bc2cb32115bbd8846
Eccenux <eccenux+git@gmail.com>
2017-06-23 02:23:01 +0200

    max_allowed_packet check

--- sys-test.php
+++ sys-test.php
@@ -1,3 +1,14 @@
+<?php
+	date_default_timezone_set('Europe/Paris');
+	function formatBytes($size, $precision = 2)
+	{
+		$size = intval($size);
+		$base = log($size, 1024);
+		$suffixes = array('', 'K', 'M', 'G', 'T');
+
+		return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +19,9 @@
 </head>
 <body lang="en">
 <div id='content'>
+
+<section>
+<h2>PHP</h2>
 <table border="1">
 <tr><th>ini key</th><th>ini value</th><th>expected</th></tr>
 <?php
@@ -35,6 +49,26 @@ foreach ($phpIniChecks as $iniKey => $expected) {
 }
 ?>
 </table>
+</section>
+
+<section>
+	<h2>MySQL</h2>
+	<table border="1">
+	<tr><th>variable name</th><th>ini value</th><th>expected</th></tr>
+	<?php
+		require_once ('./inc/dbConnect.php');
+		$pv_result = mysql_query("show variables like 'max_allowed_packet'");
+		$sqlIniChecks = array(
+			'max_allowed_packet' => '>=50 MB'
+		);
+		while ($row = mysql_fetch_array($pv_result, MYSQL_ASSOC))
+		{
+			$expected = $sqlIniChecks[$row['Variable_name']];
+			echo "\n<tr><td>{$row['Variable_name']}</td><td>".formatBytes($row['Value'])."B</td><td>$expected</td></tr>";
+		}
+	?>
+	</table>
+</section>
 </div>
 </body>
 </html>
