ce80ee23a938085bc4ebcaf326ab513e9362a385
Eccenux <eccenux+git@gmail.com>
2017-07-02 22:48:03 +0200

    visual distinction for messages

--- _install.bat
+++ _install.bat
@@ -3,7 +3,7 @@
 cls
 
 rem Test encoding
-rem echo Za¿ó³æ gêœl¹ jaŸñ!
+rem echo   Za¿ó³æ gêœl¹ jaŸñ!
 rem PAUSE
 rem GOTO :EOF
 
@@ -25,32 +25,32 @@ IF EXIST "__settings.bat" (
 )
 
 echo.
-echo To jest instalator (i aktualizator) skryptów POLos.
-echo Uwaga! Nie wykonuj aktualizacji jeœli nie musisz.
+echo  /—————————
+echo   To jest instalator (i aktualizator) skryptów POLos.
+echo   Uwaga! Nie wykonuj aktualizacji jeœli nie musisz.
 echo.
-echo Aby przerwaæ instalacjê zamknij to okno.
+echo   Aby przerwaæ instalacjê zamknij to okno.
 echo.
-echo Instalator bêdzie operowa³ na plikach w poni¿szym folderze.
-echo Musisz mieæ uprawnienia do modyfikacji tego folderu!
-echo %tmpset_INSTALL_DIR%
+echo   Instalator bêdzie operowa³ na plikach w poni¿szym folderze.
+echo   Musisz mieæ uprawnienia do modyfikacji tego folderu!
+echo   %tmpset_INSTALL_DIR%
+echo  \—————————
 echo.
 PAUSE
 
 rem
 rem Initial check
 rem
-echo.
-echo Sprawdzenie narzêdzi.
-git --version
+git --version > NUL
 IF [%ERRORLEVEL%] NEQ [0] (
 	echo.
-	echo B³¹d! Nie uda³o siê uruchomiæ git. Musisz zainstalowaæ Git dla Windows.
+	echo  /—————————
+	echo   B³¹d! Nie uda³o siê uruchomiæ git. Musisz zainstalowaæ Git dla Windows.
+	echo  \—————————
 	echo.
 	PAUSE
 	GOTO :EOF
 )
-echo OK.
-echo.
 
 rem
 rem Installation type
@@ -69,7 +69,9 @@ rem
 mkdir "%tmpset_INSTALL_DIR%"
 IF [%ERRORLEVEL%] NEQ [0] (
 	echo.
-	echo B³¹d! Nie uda³o utworzyæ folderu instalacyjnego. Spróbuj uruchomiæ skrypt z uprawnieniami administratora.
+	echo  /—————————
+	echo   B³¹d! Nie uda³o utworzyæ folderu instalacyjnego. Spróbuj uruchomiæ skrypt z uprawnieniami administratora.
+	echo  \—————————
 	echo.
 	PAUSE
 	GOTO :EOF
@@ -85,7 +87,9 @@ rem
 git clone %tmpset_GIT_REPO% .
 IF [%ERRORLEVEL%] NEQ [0] (
 	echo.
-	echo B³¹d! Nie uda³o siê pobraæ danych. Upewnij siê, ¿e masz po³¹czenie z Internetem i spróbuj ponownie.
+	echo  /—————————
+	echo   B³¹d! Nie uda³o siê pobraæ danych. Upewnij siê, ¿e masz po³¹czenie z Internetem i spróbuj ponownie.
+	echo  \—————————
 	echo.
 	PAUSE
 	GOTO :EOF
@@ -95,10 +99,12 @@ rem temp dir
 mkdir .\temp
 
 echo.
-echo Wygl¹da na to, ¿e instalacja skryptów POLos powiod³a siê.
-echo Uwaga! Pamiêtaj, ¿e oprócz skryptów POLos potrzebujesz tak¿e oprogramowania serwerowego (tj. Apache, PHP, MySQL).
+echo  /—————————
+echo   Wygl¹da na to, ¿e instalacja skryptów POLos powiod³a siê.
+echo   Uwaga! Pamiêtaj, ¿e oprócz skryptów POLos potrzebujesz tak¿e oprogramowania serwerowego (tj. Apache, PHP, MySQL).
 echo.
-echo Kontynuuj aby otworzyæ instrukcjê instalacji oprogramowania serwerowego.
+echo   Kontynuuj aby otworzyæ instrukcjê instalacji oprogramowania serwerowego.
+echo  \—————————
 echo.
 PAUSE
 start "" "https://github.com/Eccenux/POLos/blob/master/INSTALL.md"
@@ -142,14 +148,18 @@ goto :EOF
 
 :access_fail
 echo.
-echo B³¹d! Nie masz uprawnieñ do modyfikacji folderu instalacyjnego. Spróbuj uruchomiæ skrypt z uprawnieniami administratora.
+echo  /—————————
+echo   B³¹d! Nie masz uprawnieñ do modyfikacji folderu instalacyjnego. Spróbuj uruchomiæ skrypt z uprawnieniami administratora.
+echo  \—————————
 echo.
 PAUSE
 GOTO :EOF
 
 :update_with_git
 echo.
-echo Znaleziono poprzednie pliki. Nast¹pi próba aktualizacji.
+echo  /—————————
+echo   Znaleziono poprzednie pliki. Nast¹pi próba aktualizacji.
+echo  \—————————
 echo.
 PAUSE
 
@@ -159,7 +169,9 @@ rem
 git fetch
 IF [%ERRORLEVEL%] NEQ [0] (
 	echo.
-	echo B³¹d! Nie uda³o siê pobraæ danych. Upewnij siê, ¿e masz po³¹czenie z Internetem i spróbuj ponownie.
+	echo  /—————————
+	echo   B³¹d! Nie uda³o siê pobraæ danych. Upewnij siê, ¿e masz po³¹czenie z Internetem i spróbuj ponownie.
+	echo  \—————————
 	echo.
 	PAUSE
 	GOTO :EOF
@@ -175,7 +187,9 @@ set /p tmpset_MODIFIEDLOG=<temp\%tmpset_TODAY%_locally_modifiedlog.count.txt
 IF [%tmpset_MODIFIEDLOG%] NEQ [0] (
 	move .err.log .err--%tmpset_TODAY%.log
 	echo.
-	echo Uwaga! Znaleziono zmieniony log b³êdów. Jego nazwa zosta³a zmieniona na .err--%tmpset_TODAY%.log
+	echo  /—————————
+	echo   Uwaga! Znaleziono zmieniony log b³êdów. Jego nazwa zosta³a zmieniona na .err--%tmpset_TODAY%.log
+	echo  \—————————
 	echo.
 )
 
@@ -188,16 +202,20 @@ set /p tmpset_MODIFIED=<temp\%tmpset_TODAY%_locally_modified.count.txt
 set /p tmpset_DELETED=<temp\%tmpset_TODAY%_locally_deleted.count.txt
 IF [%tmpset_MODIFIED%] NEQ [0] (
 	echo.
-	echo Uwaga! Znaleziono zmienione pliki. Poni¿sze pliki zostan¹ nadpisane.
-	echo Aby dokoñczyæ aktualizacjê zalecane jest nadpisanie plików.
+	echo  /—————————
+	echo   Uwaga! Znaleziono zmienione pliki. Poni¿sze pliki zostan¹ nadpisane.
+	echo   Aby dokoñczyæ aktualizacjê zalecane jest nadpisanie plików.
 	type temp\%tmpset_TODAY%_locally_modified.txt
+	echo  \—————————
 	echo.
 )
 IF [%tmpset_DELETED%] NEQ [0] (
 	echo.
-	echo Uwaga! Znaleziono usuniête pliki. Poni¿sze pliki zostan¹ przywrócone.
-	echo Aby dokoñczyæ aktualizacjê zalecane jest przywrócenie plików.
+	echo  /—————————
+	echo   Uwaga! Znaleziono usuniête pliki. Poni¿sze pliki zostan¹ przywrócone.
+	echo   Aby dokoñczyæ aktualizacjê zalecane jest przywrócenie plików.
 	type temp\%tmpset_TODAY%_locally_deleted.txt
+	echo  \—————————
 	echo.
 )
 
@@ -208,7 +226,9 @@ rem Note! Files that are not in the repo will NOT be removed.
 rem Ignored files will NOT be removed.
 IF [%tmpset_MODIFIED%%tmpset_DELETED%] NEQ [00] (
 	echo.
-	echo Kontynuuj jeœli mo¿na nadpisaæ/przywróciæ pliki.
+	echo  /—————————
+	echo   Kontynuuj jeœli mo¿na nadpisaæ/przywróciæ pliki.
+	echo  \—————————
 	echo.
 	PAUSE
 	git reset --hard origin/master
@@ -218,8 +238,10 @@ GOTO update_end
 
 :update_no_git
 echo.
-echo Znaleziono pliki, które nie by³y aktualizowane. Niektóre pliki mog¹ zostaæ nadpisane.
-echo Aby wykonaæ aktualizacjê konieczne jest nadpisanie plików.
+echo  /—————————
+echo   Znaleziono pliki, które nie by³y aktualizowane. Niektóre pliki mog¹ zostaæ nadpisane.
+echo   Aby wykonaæ aktualizacjê konieczne jest nadpisanie plików.
+echo  \—————————
 echo.
 PAUSE
 
@@ -228,7 +250,9 @@ git remote add origin %tmpset_GIT_REPO%
 git fetch
 IF [%ERRORLEVEL%] NEQ [0] (
 	echo.
-	echo B³¹d! Nie uda³o siê pobraæ danych. Upewnij siê, ¿e masz po³¹czenie z Internetem i spróbuj ponownie.
+	echo  /—————————
+	echo   B³¹d! Nie uda³o siê pobraæ danych. Upewnij siê, ¿e masz po³¹czenie z Internetem i spróbuj ponownie.
+	echo  \—————————
 	echo.
 	PAUSE
 	GOTO :EOF
@@ -240,12 +264,14 @@ GOTO update_end
 
 :update_end
 echo.
-echo Wygl¹da na to, ¿e aktualizacja skryptów POLos powiod³a siê.
+echo  /—————————
+echo   Wygl¹da na to, ¿e aktualizacja skryptów POLos powiod³a siê.
 echo.
-echo Kontynuuj aby uruchomiæ POLos.
+echo   Kontynuuj aby uruchomiæ POLos.
 echo.
-echo Uwaga! Jeœli otworzy siê strona z b³êdem, to upewnij siê, ¿e oprogramowanie serwerowe jest prawid³owo zainstalowane.
-echo Jeœli oprogramowanie jest zainstalowane, to sprawdŸ logi.
+echo   Uwaga! Jeœli otworzy siê strona z b³êdem, to upewnij siê, ¿e oprogramowanie serwerowe jest prawid³owo zainstalowane.
+echo   Jeœli oprogramowanie jest zainstalowane, to sprawdŸ logi.
+echo  \—————————
 echo.
 PAUSE
 start "" "%tmpset_POLOS_URL%"
